3453518c0e8c0e139bd5179e9fd6825980ec2b3a
Adeyemo Samuel <adeyemosamuel@gmail.com>
2018-03-13 13:42:04 +0100

    corp

--- src/providers/server-service/server-service.ts
+++ src/providers/server-service/server-service.ts
@@ -3,6 +3,7 @@ import { Http, Headers } from '@angular/http';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/timeout';
 import 'rxjs/add/operator/toPromise';
+import { Storage } from '@ionic/storage/dist/storage';
 
 @Injectable()
 export class ServerServiceProvider {
@@ -11,16 +12,16 @@ export class ServerServiceProvider {
  
   url: string = '/api';
 
-  constructor(public http: Http) {
+  constructor(public http: Http, private store: Storage) {
     console.log('Hello ServerService Provider');
   }
 //post api
   async processData(body, funcName): Promise<any> {
     console.log(body);
     console.log(funcName);
-
+    // let varToken= await this.store.get('token')
     let headers = new Headers();
-    headers.append('Content-Type', 'application/json');
+  headers.append('Content-Type', 'application/json'); 
 
     try {
       let response = await this.http.post(this.url + funcName, JSON.stringify(body), {headers: headers}).toPromise();
